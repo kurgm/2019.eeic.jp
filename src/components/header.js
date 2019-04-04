3e3c59b1db9a2c72f9b22310ce1024cb42cb33cd
kurgm <gogottt2009-hp@yahoo.co.jp>
2019-04-04 13:25:41 +0900

    Add icon in title and favicon

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,53 @@
-import { Link } from "gatsby"
+import { Link, StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header>
-    <div>
-      <h1>
-        <Link to="/">
-          {siteTitle}
-        </Link>
-      </h1>
-      <nav>
-        <Link to="/page-2/">
-          Page two
-        </Link>
-        <Link to="/page-2/">
-          Page two
-        </Link>
-        <Link to="/page-2/">
-          Page two
-        </Link>
-        <Link to="/page-2/">
-          Page two
-        </Link>
-      </nav>
-    </div>
-  </header>
+  <StaticQuery
+    query={graphql`
+      query {
+        iconImage: file(relativePath: { eq: "eeic-black.png" }) {
+          childImageSharp {
+            fixed(height: 50) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    `}
+    render={data =>
+      <header>
+        <div>
+          <h1>
+            <Link to="/">
+              <Img
+                fixed={data.iconImage.childImageSharp.fixed}
+                className="header-icon"
+              />
+              <div>
+                {siteTitle}
+              </div>
+            </Link>
+          </h1>
+          <nav>
+            <Link to="/page-2/">
+              Page two
+            </Link>
+            <Link to="/page-2/">
+              Page two
+            </Link>
+            <Link to="/page-2/">
+              Page two
+            </Link>
+            <Link to="/page-2/">
+              Page two
+            </Link>
+          </nav>
+        </div>
+      </header>
+    }
+  />
 )
 
 Header.propTypes = {
