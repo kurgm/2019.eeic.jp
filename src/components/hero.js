583e6aabd8acecd49117a338f18bfac86d19fd86
kurgm <gogottt2009-hp@yahoo.co.jp>
2019-04-06 23:36:21 +0900

    Hero background

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,17 +1,40 @@
 import PropTypes from "prop-types"
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import BackgroundImage from 'gatsby-background-image'
 
 const Hero = ({ pageTitle, pageSubtitle, height, children }) => (
-  <section className="hero" style={{height}}>
-    {children || <>
-      <h2>
-        {pageTitle}
-      </h2>
-      <p className="subtitle">
-        {pageSubtitle}
-      </p>
-    </>}
-  </section>
+  <StaticQuery
+    query={graphql`
+      query {
+        bgimage: file(relativePath: { eq: "toppic.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 4160) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <section className="hero" style={{height}}>
+        <BackgroundImage
+          className="hero-blur"
+          fluid={data.bgimage.childImageSharp.fluid}
+          style={{ position: `absolute` }}
+          backgroundColor="#10457E"
+        />
+        {children || <>
+          <h2>
+            {pageTitle}
+          </h2>
+          <p className="subtitle">
+            {pageSubtitle}
+          </p>
+        </>}
+      </section>
+    )}
+  />
 )
 
 Hero.propTypes = {
