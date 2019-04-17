ae32d813d96bd7e614cf6c8a2f7d9f6aef5bca36
kurgm <gogottt2009-hp@yahoo.co.jp>
2019-04-17 13:22:46 +0900

    Add htaccess files

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,24 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const fs = require(`fs`)
+const path = require(`path`)
+
+const htFiles = [
+  { src: `src/htfiles/htaccess`, dest: `.htaccess` },
+  { src: `src/htfiles/static-htaccess`, dest: `static/.htaccess` },
+]
+
+exports.onPostBuild = async ({ store }) => {
+  const { program } = store.getState()
+  await Promise.all(htFiles.map(({ src, dest }) => {
+    const srcPath = path.join(program.directory, src)
+    const destPath = path.join(program.directory, `public`, dest)
+    return new Promise((resolve, reject) => {
+      fs.createReadStream(srcPath)
+        .pipe(fs.createWriteStream(destPath))
+        .on('finish', resolve)
+        .on('error', reject)
+    })
+  }))
+}
